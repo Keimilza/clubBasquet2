1ea8f3eda59672f2dbae3280558b03d0530cdf24
keimilza <keimilza@gmail.com>
2019-07-02 11:17:28 +0200

    Instalacion de paquetes

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/",name="home")   
+     */
+    public function homepage()
+    {
+       ///return new Response('Mi primera pagina');   
+       return $this->render('home/home.html.twig');  
+    }
+}
